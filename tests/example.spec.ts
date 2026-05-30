768a282640260fdead502c226ea690e598c0db81
nirmalrani82 <nirmalrani061198@gmail.com>
2026-05-30 12:54:11 +0530

    Second commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('understand fixtures', async ({ page, browser }) => {
+
+  console.log(browser);
+
+  await page.goto('https://playwright.dev/');
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
