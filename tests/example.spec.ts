8182e90072b8679da0516a3cc3707e98ad949c6f
nirmalrani82 <nirmalrani061198@gmail.com>
2026-05-30 13:04:48 +0530

    Added a console log statement

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,9 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('understand fixtures', async ({ page, browser }) => {
 
-  console.log(browser);
+	console.log(browser);
 
-  await page.goto('https://playwright.dev/');
+  	await page.goto('https://playwright.dev/');
 
-  await expect(page).toHaveTitle(/Playwright/);
+   	console.log("Hello world");
+
+  	await expect(page).toHaveTitle(/Playwright/);
 });
