d46f92818431c0de4a341f1cc4ecefaf04ca5364
nirmalrani82 <nirmalrani061198@gmail.com>
2026-05-30 13:23:19 +0530

    another commit message

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,5 +6,7 @@ test('understand fixtures', async ({ page, browser }) => {
 
   	await page.goto('https://playwright.dev/');
 
+	console.log("Another hello world");
+
   	await expect(page).toHaveTitle(/Playwright/);
 });
